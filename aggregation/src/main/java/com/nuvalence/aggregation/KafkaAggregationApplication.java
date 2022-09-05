03530fa11810f2d75ebcdaab04362dce1e0d215d
James Grubb <jamesgrubb@NUVALENCE-MB25.local>
2022-09-05 17:57:48 -0400

    Project init

---
+++ aggregation/src/main/java/com/nuvalence/aggregation/KafkaAggregationApplication.java
@@ -0,0 +1,13 @@
+package com.nuvalence.aggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAggregationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAggregationApplication.class, args);
+	}
+
+}
---
+++ aggregation/src/test/java/com/nuvalence/aggregation/KafkaAggregationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuvalence.aggregation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAggregationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
